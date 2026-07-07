0f4d8293ae959ecb0f37660c7b52f0dde0f05162
Dan Graboi <dgraboi@sbcglobal.net>
2026-07-07 16:47:23 -0700

    Live build 20260707d

--- globe/globe.js
+++ globe/globe.js
@@ -32,7 +32,7 @@
   }
 
   const query = dataApi.parseQueryParams();
-  const fallbackHomeUrl = `../telepathybeginner.html?v=${encodeURIComponent(query.version || "20260707c")}&open=launcher`;
+  const fallbackHomeUrl = `../telepathybeginner.html?v=${encodeURIComponent(query.version || "20260707d")}&open=launcher`;
   let globe = null;
   let renderedConnections = [];
   let renderedPoints = [];
--- telepathybeginner-sw.js
+++ telepathybeginner-sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "telepathybeginner-v20260707c";
-const APP_VERSION = "20260707c";
+const CACHE_NAME = "telepathybeginner-v20260707d";
+const APP_VERSION = "20260707d";
 const APP_LAUNCH_URL = `./telepathybeginner.html?v=${APP_VERSION}&open=launcher`;
 const APP_ASSETS = [
   "./",
@@ -23,7 +23,7 @@ const APP_ASSETS = [
   "./tb-test-icon-3.png",
   "./tb-test-icon-4.png",
   "./BeginnerUserManual.html",
-  `./BeginnerUserManual.html?v=20260707c`,
+  `./BeginnerUserManual.html?v=20260707d`,
   "./minds-connected-uncropped.png",
   "./rewire.png",
   "./RV1.png",
