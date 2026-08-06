8b430bccc491a2d27db91dd24e90dbe206f90e28
Dan Graboi <dgraboi@sbcglobal.net>
2026-08-05 18:01:29 -0700

    Update landing page copy and deploy 20260806a

--- globe/globe.js
+++ globe/globe.js
@@ -32,7 +32,7 @@
   }
 
   const query = dataApi.parseQueryParams();
-  const fallbackHomeUrl = `../telepathybeginner.html?v=${encodeURIComponent(query.version || "20260714b")}&open=launcher`;
+  const fallbackHomeUrl = `../telepathybeginner.html?v=${encodeURIComponent(query.version || "20260806a")}&open=launcher`;
   let globe = null;
   let renderedConnections = [];
   let renderedPoints = [];
